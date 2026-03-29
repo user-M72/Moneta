1f6dfa7465cfd4e329b7ab7401877a5b1d758c94
LinKord Azamov <muzaffarazamov007@gmail.com>
2026-03-29 22:12:32 +0500

    manag your money

---
+++ src/main/java/Moneta/cashflow/CashflowApplication.java
@@ -0,0 +1,13 @@
+package Moneta.cashflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashflowApplication.class, args);
+	}
+
+}
