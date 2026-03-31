73015843f6ed549d191f5a25a7d4d0b2330ec5de
user-M72 <muzaffarazamov007@gmail.com>
2026-03-31 13:22:17 +0500

    update RoleMapper class added void method

--- src/main/java/Moneta/cashflow/mapper/RoleMapper.java
+++ src/main/java/Moneta/cashflow/mapper/RoleMapper.java
@@ -5,6 +5,8 @@ import Moneta.cashflow.dto.role.RoleReqDto;
 import Moneta.cashflow.dto.role.RoleResDto;
 import Moneta.cashflow.entity.Role;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
@@ -13,4 +15,6 @@ public interface RoleMapper {
     RoleResDto toDto(Role role);
 
     Role toEntity(RoleReqDto dto);
+
+    void updatedFromDto(RoleReqDto dto, @MappingTarget Role role);
 }
